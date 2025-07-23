3bd4660fe236fd3d352378f4040e96484184d395
kekedezha <dezhac6@gmail.com>
2025-07-23 16:45:51 -0400

    updating packages for project

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom'; // import route provider 'BrowserRouter'
 import App from './App.tsx'
 import './index.css'
 
