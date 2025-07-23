26e7ba09602a3d29459f3c5c044885813e20b9ad
kekedezha <dezhac6@gmail.com>
2025-07-23 17:51:40 -0400

    added button from shadcn library

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,18 @@ import type { Config } from "tailwindcss";
 const config: Config = {
   content: ["./index.html", "./src/**/*.{ts,tsx,js,jsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        cream: "#FFF6E5",
+        cocoa: "#A47148",
+        crust: "#F4D9AE",
+        pinkfrost: "#F8D3D3",
+      },
+      fontFamily: {
+        display: ['"Playfair Display"', "serif"],
+        sans: ['"Inter"', "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
