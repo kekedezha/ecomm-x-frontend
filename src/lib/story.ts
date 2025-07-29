273698b6c5f86e2da66c8c20df0d8be01c97f1c8
kekedezha <dezhac6@gmail.com>
2025-07-29 16:03:52 -0400

    added/updated strings to be exported to Story page

--- src/lib/story.ts
+++ src/lib/story.ts
@@ -1,5 +1,17 @@
-export const general: string =
-  "Born of air, time, and craft - Alvéole is a celebration of the delicate precision behind every loaf, every layer, every crumb. Alvéole is a cozy and charming local bakehouse in the Wesley Heights neighborhood of Charlotte, NC. Best known for our croissants variety of yummy pastries. Additionally, we feature an array of freshly baked bread, cookies, and tarts that showcase both traditional recipes and more current, innovative flavors. \nOur customers are drawn to our warm atmosphere that properly reflects our neighborhood of Wesley Heights, friendly staff and the delightful aroma of baked goods that fills the air. With our focus on quality ingredients and attention to detail, Alvéole has become a beloved spot for both locals and visitors seeks a taste of homemade goodness.";
+export const general1: string =
+  "Alvéole is a cozy and charming local bakehouse in the Wesley Heights neighborhood of Charlotte, NC. Best known for our croissants variety of yummy pastries. Additionally, we feature an array of freshly baked bread, cookies, and tarts that showcase both traditional recipes and more current, innovative flavors.";
 
-export const passion: string =
-  "Christian's love for baked goods and baking came from a combination of his dad's love of baked goods and the love his mom had for cooking and hospitality. \nOne of Christian's fondest memory growing up was having breakfast with his parents on the weekends. This was usually the time where his whole came together as a whole and ate breakfast together. Not one weekend would go by where his dad did not have a baked good in his hands with cup of coffee. Nowadays he cherishes bringing croissants or some pastries back when he visits his parents to share a nice cup of coffee. \n";
+export const general2: string =
+  "Our customers are drawn to our warm atmosphere that properly reflects our neighborhood of Wesley Heights, friendly staff and the delightful aroma of baked goods that fills the air. With our focus on quality ingredients and attention to detail, Alvéole has become a beloved spot for both locals and visitors seeks a taste of homemade goodness.";
+
+export const passion1: string =
+  "Christian's love for baked goods and baking came from a combination of his dad's love of baked goods, the love his mom had for cooking and hospitality, and ";
+
+export const passion2: string =
+  "One of Christian's fondest memory growing up was having breakfast with his parents on the weekends. This was usually the time where his whole came together as a whole and ate breakfast together. Not one weekend would go by where, either before or after breakfast, his dad did not have a baked good in his hands alongside a cup of coffee. Nowadays he cherishes bringing croissants or some pastries back when he visits his parents to indulge and relive those nostalgic memories.";
+
+export const passion3: string =
+  "t was only later, when shift careers, that he began to explore the art of baking. Christian loved learning new things and doing things he had a passion and sweet tooth for. It served to him as a tool and outlet to move away from being around coding all the time. Having a eye and almost an innate feeling sense for cooking has allowed Christian to work and succeed at being apart of different bake shops despite not having a culinary education.";
+
+export const homePageQuote: string =
+  "Born of air, time, and craft - Alvéole is a celebration of the delicate precision behind every loaf, every layer, every crumb.";
