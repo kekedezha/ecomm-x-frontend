3f267b79c15d506db62b6d8e873d0ef9c2bc3b37
kekedezha <dezhac6@gmail.com>
2025-07-30 17:54:50 -0400

    added footer, still work in progress

---
+++ src/components/layout/Footer.tsx
@@ -0,0 +1,12 @@
+
+export function Footer() {
+    return (
+        <footer className="bg-blue-600 py-8 z-0 h-60 m">
+            <div className="flex justify-between px-8">
+                <p>Copyright 2025 | Alvéole - Pain & Pâtisserie</p>
+                <p>Terms of Use | Privacy Policy</p>
+                <p>Made by Christian Dezha</p>
+            </div>
+        </footer>
+    )
+}
