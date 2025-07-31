b048cafb7d79fb8f8cfd66d6bdafc28aa18454bb
kekedezha <dezhac6@gmail.com>
2025-07-31 18:40:21 -0400

    updated footer, work in progress

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,8 +1,8 @@
 
 export function Footer() {
     return (
-        <footer className="bg-blue-600 py-8 z-0 h-60 m">
-            <div className="flex justify-between px-8">
+        <footer className="bg-blue-600 h-60 flex items-end py-4 px-8 z-0">
+            <div className="flex justify-between px-8 w-full">
                 <p>Copyright 2025 | Alvéole - Pain & Pâtisserie</p>
                 <p>Terms of Use | Privacy Policy</p>
                 <p>Made by Christian Dezha</p>
