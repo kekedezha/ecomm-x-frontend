dbc8d74abac43696163b38fb0dd9a77e3c8dca9e
kekedezha <dezhac6@gmail.com>
2025-08-15 17:15:12 -0400

    working progress on contact page. 75% complete

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import contactImage from '../assets/images/pinar-kucuk-3-PfUgbyWEs-unsplash.jpg';
 
 import { zodResolver } from '@hookform/resolvers/zod';
@@ -13,6 +15,7 @@ import {
     FormItem,
     FormMessage,
 } from "@/components/ui/form";
+import { Textarea } from '@/components/ui/textarea';
 import { Input } from '@/components/ui/input';
 
 // Form schema
@@ -20,7 +23,8 @@ const formSchema = z.object({
     firstName: z.string(),
     lastName: z.string(),
     phoneNumber: z.string().length(10, "Phone number must be include area code!"),
-    email: z.email("Please enter a proper email.")
+    email: z.email("Please enter a proper email."),
+    comments: z.string().max(160, "Comments must not be longer than 160 characters.").optional(),
 })
 
 
@@ -32,21 +36,22 @@ export default function Contact() {
             firstName: "",
             lastName: "",
             phoneNumber: "",
-            email: ""
+            email: "",
+            comments: ""
         }
     })
 
+    // 2. Define a submit handler 
     function onSubmit(values: z.infer<typeof formSchema>) {
-        toast(`${values.firstName}, thanks for submitting the following details! We'll keep in touch.`, {
+        toast.info(`${values.firstName}, thanks for submitting the following details! We'll keep in touch.`, {
             description: (
                 <div>
                     <p>First Name: {values.firstName}</p>
-                    <p>Last Name: {values.lastName}</p>
-                    <p>Phone Number: {values.phoneNumber}</p>
                     <p>Email: {values.email}</p>
                 </div>
             )
         })
+        console.log(values);
     }
 
     return (
@@ -61,7 +66,9 @@ export default function Contact() {
                                     control={form.control}
                                     name="firstName"
                                     render={({ field }) => (
-                                        <FormItem>
+                                        <FormItem
+                                            className='basis-[calc(50%_-_2rem)]'
+                                        >
                                             <FormControl>
                                                 <Input placeholder='First Name' {...field} />
                                             </FormControl>
@@ -71,7 +78,9 @@ export default function Contact() {
                                     control={form.control}
                                     name="lastName"
                                     render={({ field }) => (
-                                        <FormItem>
+                                        <FormItem
+                                            className='basis-[calc(50%_-_2rem)]'
+                                        >
                                             <FormControl>
                                                 <Input placeholder='Last Name' {...field} />
                                             </FormControl>
@@ -81,7 +90,9 @@ export default function Contact() {
                                     control={form.control}
                                     name="phoneNumber"
                                     render={({ field }) => (
-                                        <FormItem>
+                                        <FormItem
+                                            className='basis-[calc(50%_-_2rem)]'
+                                        >
                                             <FormControl>
                                                 <Input type="tel" placeholder='Phone Number' {...field} />
                                             </FormControl>
@@ -92,23 +103,47 @@ export default function Contact() {
                                     control={form.control}
                                     name="email"
                                     render={({ field }) => (
-                                        <FormItem>
+                                        <FormItem
+                                            className='basis-[calc(50%_-_2rem)]'
+                                        >
                                             <FormControl>
                                                 <Input placeholder='Email' {...field} />
                                             </FormControl>
                                             <FormMessage />
                                         </FormItem>
                                     )} />
-                                <Button type='submit'>Submit</Button>
+                                <FormField
+                                    control={form.control}
+                                    name="comments"
+                                    render={({ field }) => (
+                                        <FormItem
+                                            className='basis-[calc(100%_-_2rem)] min-h-[30vh]'
+                                        >
+                                            <FormControl>
+                                                <Textarea placeholder='Tell us what you need...' {...field} />
+                                            </FormControl>
+                                            <FormMessage />
+                                        </FormItem>
+                                    )} />
+                                <div className='basis-full py-0 px-8 flex content-start'>
+                                    <div className='fit-content relative overflow-hidden cursor-pointer'>
+                                        <Button className='outline-none padding cursor-pointer text-2xl uppercase w-full'
+                                            type="submit"
+                                        >
+                                            Send
+                                        </Button>
+                                    </div>
+                                </div>
+
                             </form>
                         </Form>
                     </div>
-                    <div className='w-full basis-[40%] relative overflow-hidden rounded-3xl aspect-[3/4]'>
-                        <div className='w-full h-[120%] bottom-0 left-0 absolute box-border leading-1'>
+                    <div className='w-full h-full basis-[40%] relative overflow-hidden rounded-3xl aspect-[3/4] box-border leading-1'>
+                        <div className='translate-none rotate-none scale-none transform-[translate(0px, 13.3567%, 0px)] w-full h-[120%] bottom-0 left-0 absolute box-border leading-1'>
                             <img
                                 src={contactImage}
                                 alt="Beautiful shop front. Photo by Pinar Kucuk on Unsplash."
-                                className="rounded-md w-full h-full object-cover box-border leading-1"
+                                className="rounded-md w-full object-cover box-border leading-1"
                             />
                         </div>
                     </div>
