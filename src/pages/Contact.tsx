ac3afd24834b96189aea167df6aec33f09080f8e
kekedezha <dezhac6@gmail.com>
2025-09-02 15:54:22 -0400

    added in email discount section at the bottom of page

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -28,6 +28,7 @@ const formSchema = z.object({
 })
 
 
+
 export default function Contact() {
     // 1. Define form
     const form = useForm<z.infer<typeof formSchema>>({
@@ -50,8 +51,11 @@ export default function Contact() {
                     <p>Email: {values.email}</p>
                 </div>
             )
-        })
-        console.log(values);
+        });
+    }
+
+    function onEmailDiscount() {
+        toast.info('Thanks for subscribing to our newsletter. An email with a 15% discount code will be send to you shortly!')
     }
 
     return (
@@ -146,6 +150,28 @@ export default function Contact() {
                         />
                     </div>
                 </section>
+                <div className=''>
+                    <section className='rounded-[0 0 1.5rem 1.5rem] relative z-2 mt-[-1px] py-[50px] px-[4vw] overflow-x-hidden'>
+                        <div className='bg-[#274472] rounded-4xl py-8 px-16 w-[60%] mx-auto flex flex-col items-center relative'>
+                            <h2 className='text-white text-6xl text-center font-cherry mb-6'>GET 15% OFF YOUR FIRST ORDER</h2>
+                            <p className='text-white leading-5 mb-6'>Subscribe to our newsletter and get 15% off your first purchase!</p>
+                            <div className="flex gap-2">
+                                <input
+                                    type="email"
+                                    placeholder="EMAIL"
+                                    className="flex-1 px-4 py-3 rounded-full bg-pink-300 placeholder-white text-white border-none focus:outline-none focus:ring-2 focus:ring-white"
+                                />
+                                <button
+                                    onClick={onEmailDiscount}
+                                    className="px-6 py-3 bg-gray-800 text-white rounded-full font-semibold hover:bg-gray-700 transition-colors"
+                                    type='button'
+                                >
+                                    JOIN
+                                </button>
+                            </div>
+                        </div>
+                    </section>
+                </div>
             </div>
         </div>
     );
