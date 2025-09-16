973c86fa844c5922b0d21e36f975cd13acaa3832
kekedezha <dezhac6@gmail.com>
2025-09-16 15:17:01 -0400

    updated width of 15% box

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -152,7 +152,7 @@ export default function Contact() {
                 </section>
                 <div className=''>
                     <section className='rounded-[0 0 1.5rem 1.5rem] relative z-2 mt-[-1px] py-[50px] px-[4vw] overflow-x-hidden'>
-                        <div className='bg-[#274472] rounded-4xl py-8 px-16 w-[60%] mx-auto flex flex-col items-center relative'>
+                        <div className='bg-[#274472] rounded-4xl py-8 px-16 w-[80%] mx-auto flex flex-col items-center relative'>
                             <h2 className='text-white text-6xl text-center font-cherry mb-6'>GET 15% OFF YOUR FIRST ORDER</h2>
                             <p className='text-white leading-5 mb-6'>Subscribe to our newsletter and get 15% off your first purchase!</p>
                             <div className="flex gap-2">
